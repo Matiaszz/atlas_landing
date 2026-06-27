4446daa0549824dd857ab456dd14a2101b7caa53
Allan Matias <alangiovanepaes@gmail.com>
2026-06-27 17:45:52 -0300

    fix: preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     siteName: "Atlas",
     images: [
       {
-        url: "/og-image.png",
+        url: "/atlas-sbg.svg",
         width: 1200,
         height: 630,
         alt: "Atlas",
